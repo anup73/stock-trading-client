5e9184a2458c5a95b874ebfc017a3bb2546145d5
anup73 <wadekaranup@gmail.com>
2026-03-14 13:18:04 +0530

    commit

---
+++ src/main/java/com/javatechie/stock_trading_client/StockTradingClientApplication.java
@@ -0,0 +1,13 @@
+package com.javatechie.stock_trading_client;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StockTradingClientApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(StockTradingClientApplication.class, args);
+	}
+
+}
